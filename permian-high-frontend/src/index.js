880c098673ad6625d4ccf4caed1a08c42cd80aae
Jeff Carroll <jcarr048@gmail.com>
2022-11-21 10:25:14 -0500

    working on homepage

--- permian-high-frontend/src/index.js
+++ permian-high-frontend/src/index.js
@@ -4,8 +4,4 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 const root = createRoot(document.getElementById('root'))
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+root.render(<App />)
--- permian-high-frontend/src/pages/home.js
+++ permian-high-frontend/src/pages/home.js
@@ -1,9 +1,18 @@
-import axios from 'axios'
-
 const Home = () => {
-
-
-    return (
-
-    )
+  return (
+    <div>
+      <div className="panther-logo"></div>
+      <div className="welcome">
+        Welcome to Permian High School, home of the Panthers!
+      </div>
+      <div className="school-grid">
+        <div className="cell1">Students</div>
+        <div className="cell2">Panther Athletics</div>
+        <div className="cell3">Permian Courses</div>
+        <div className="cell4">Permian Facebook</div>
+      </div>
+    </div>
+  )
 }
+
+export default Home
